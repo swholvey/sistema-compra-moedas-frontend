e59c7910b2aca558df1ca95531f718df1039e84e
Dheison Silva <dh3150n@gmail.com>
2024-11-11 09:14:05 -0300

    update api url

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,16 @@
   <div>
     <!-- Componente de compra, emitindo a nova transação -->
     <CurrencyPurchase @transaction-added="addTransaction" />
-    
+
     <!-- Componente de histórico de transações, recebendo a lista atualizada -->
     <TransactionHistory :transactions="transactions" />
   </div>
 </template>
 
 <script>
-import CurrencyPurchase from './components/CurrencyPurchase.vue';
-import TransactionHistory from './components/TransactionHistory.vue';
-import axios from 'axios';
+import CurrencyPurchase from './components/CurrencyPurchase.vue'
+import TransactionHistory from './components/TransactionHistory.vue'
+import axios from 'axios'
 
 export default {
   components: {
@@ -21,25 +21,25 @@ export default {
   data() {
     return {
       transactions: [],
-    };
+    }
   },
   methods: {
     // Adiciona a nova transação à lista de transações
     addTransaction(newTransaction) {
-      this.transactions.push(newTransaction);
+      this.transactions.push(newTransaction)
     },
     // Carrega as transações existentes no banco
     async fetchTransactions() {
       try {
-        const response = await axios.get('http://localhost:8000/api/transacoes');
-        this.transactions = response.data;
+        const response = await axios.get('https://sohenid.online/api/transacoes')
+        this.transactions = response.data
       } catch (error) {
-        console.error('Erro ao buscar transações:', error);
+        console.error('Erro ao buscar transações:', error)
       }
     },
   },
   mounted() {
-    this.fetchTransactions(); // Carrega as transações ao montar o componente
+    this.fetchTransactions() // Carrega as transações ao montar o componente
   },
-};
+}
 </script>
--- src/components/CurrencyPurchase.vue
+++ src/components/CurrencyPurchase.vue
@@ -2,28 +2,34 @@
   <div class="p-15 rounded-5 border">
     <form @submit.prevent="calculateTotal" class="form">
       <div class="mb-5">
-        <label class="d-block fw-bold mb-5" for="currency">
-          Moeda:
-        </label>
-        <select class="d-block w-100 rounded-5 p-15 border" v-model="currency" id="currency" required>
+        <label class="d-block fw-bold mb-5" for="currency"> Moeda: </label>
+        <select
+          class="d-block w-100 rounded-5 p-15 border"
+          v-model="currency"
+          id="currency"
+          required
+        >
           <option v-for="currency in currencies" :key="currency.code" :value="currency.code">
             {{ currency.name }}
           </option>
         </select>
       </div>
       <div class="mt-15">
-        <label class="d-block fw-bold mb-5" for="amount">
-          Quantidade R$:
-        </label>
-        <input class="d-block w-100 rounded-5 p-15 border" type="number" v-model="amount" id="amount" required min="50" />
+        <label class="d-block fw-bold mb-5" for="amount"> Quantidade R$: </label>
+        <input
+          class="d-block w-100 rounded-5 p-15 border"
+          type="number"
+          v-model="amount"
+          id="amount"
+          required
+          min="50"
+        />
       </div>
-      <button class="btn btn-primary mt-15" type="submit">
-        Calcular Total
-      </button>
+      <button class="btn btn-primary mt-15" type="submit">Calcular Total</button>
     </form>
 
-    <hr class="mt-15" v-if="total">
-    
+    <hr class="mt-15" v-if="total" />
+
     <div class="alert alert-success mt-15" v-if="total">
       <h3>Resumo da Compra</h3>
       <p class="mt-15">Total com Taxa: {{ total.total.toFixed(2) }}</p>
@@ -37,8 +43,7 @@
 </template>
 
 <script>
-
-import axios from 'axios';
+import axios from 'axios'
 
 export default {
   data() {
@@ -47,46 +52,46 @@ export default {
       amount: 50,
       currencies: [],
       total: null,
-    };
+    }
   },
   methods: {
     async fetchCurrencies() {
       try {
-        const response = await axios.get('http://localhost:8000/api/moedas');
-        this.currencies = response.data;
+        const response = await axios.get('https://sohenid.online/api/moedas')
+        this.currencies = response.data
       } catch (error) {
-        console.error('Erro ao buscar moedas:', error);
+        console.error('Erro ao buscar moedas:', error)
       }
     },
     async calculateTotal() {
       try {
-        const response = await axios.post('http://localhost:8000/api/calcula-total', {
+        const response = await axios.post('https://sohenid.online/api/calcula-total', {
           currency: this.currency,
           amount: this.amount,
-        });
-        this.total = response.data;
+        })
+        this.total = response.data
       } catch (error) {
-        console.error('Erro ao calcular total:', error);
+        console.error('Erro ao calcular total:', error)
       }
     },
     async confirmPurchase() {
       try {
-        const response = await axios.post('http://localhost:8000/api/comprar', {
+        const response = await axios.post('https://sohenid.online/api/comprar', {
           currency: this.currency,
           amount: this.amount,
-        });
-        this.$emit('transaction-added', response.data.transaction);
-        alert('Compra confirmada!');
-        this.total = null;
-        this.amount = 50;
-        this.currency = '';
+        })
+        this.$emit('transaction-added', response.data.transaction)
+        alert('Compra confirmada!')
+        this.total = null
+        this.amount = 50
+        this.currency = ''
       } catch (error) {
-        console.error('Erro ao confirmar compra:', error);
+        console.error('Erro ao confirmar compra:', error)
       }
     },
   },
   mounted() {
-    this.fetchCurrencies();
+    this.fetchCurrencies()
   },
-};
+}
 </script>
